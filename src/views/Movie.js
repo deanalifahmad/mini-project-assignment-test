50de442d64fbc7757ab462a33bb4c6e4c6ace7fd
Dean Alif Ahmad <jalal8915@outlook.com>
2023-03-01 01:54:32 +0700

    add css loader for http requests and handle errors on failed requests

--- src/views/Movie.js
+++ src/views/Movie.js
@@ -1,42 +1,71 @@
 import React, { useState, useEffect } from "react";
+import Loader from "../components/Loader";
+import Error from "../components/Error";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
 export default function Product() {
     const { id } = useParams();
     const url = `https://api.themoviedb.org/3/movie/${id}?api_key=199990f1b48c24344250a53361249e57`;
-    const [movie, setMovie] = useState(null);
+    const [movie, setMovie] = useState({
+        loading: false,
+        data: null,
+        error: false
+    });
 
     let content = null;
 
     useEffect(() => {
+        setMovie({
+            loading: true,
+            data: null,
+            error: false
+        });
         axios.get(url).then(response => {
-            setMovie(response.data);
+            setMovie({
+                loading: false,
+                data: response.data,
+                error: false
+            });
+        }).catch(() => {
+            setMovie({
+                loading: false,
+                data: null,
+                error: true
+            });
         })
     }, [url]);
 
-    if (movie) {
+    if (movie.loading) {
+        content = <Loader></Loader>
+    }
+
+    if (movie.error) {
+        content = <Error />
+    }
+
+    if (movie.data) {
         content = 
             <div className="container mx-auto p-4">
                     <h1 className="text-2xl text-center text-dark font-bold mb-3">
-                        {movie.title}
+                        {movie.data.title}
                     </h1>
                 <div className="flex flex-col items-center">
                     <div>
                         <img 
-                            src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-                            alt={movie.title}
+                            src={"https://image.tmdb.org/t/p/w500" + movie.data.poster_path}
+                            alt={movie.data.title}
                             className="h-[300px] w-[300px]"
                         />
                     </div>
                     <div className="my-3 mx-2">
                         <h2 className="font-semibold text-alerange text-justify">Release Date</h2>
                         <p className="mb-2">
-                            {movie.release_date}
+                            {movie.data.release_date}
                         </p>
                         <h2 className="font-semibold text-alerange text-justify">Overview</h2>
                         <p className="mb-2">
-                            {movie.overview}
+                            {movie.data.overview}
                         </p>
                     </div>
                 </div>
